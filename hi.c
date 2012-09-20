1943c0e5c1e85b8535edf210ab9d4ab90d23e69b
bill <billwawa@126.com>
2012-09-20 21:23:21 +0800

    a.out

--- hi.c
+++ hi.c
@@ -8,20 +8,15 @@ int main()
   char *a = "hello world..";
   char *b = NULL;
   char *c = copy(b, a);
-  printf("........................\n");
-  //printf("%s\n", c);
+  printf("[%s]\n", c);
   return 0;
 }
 
 char *copy(char *s, char *t)
 {
-  printf("....\n");
   s = (char *)malloc(100);
   char *tmp = s;
-  printf("....\n");
   for( ;(*s = *t) != '\0'; s++,t++)
-    printf("23....\n");
     ;
-  printf("%s", tmp); 
   return tmp;
 } 
