9c6ae4cfcb8dda6969beb5dfc06a5cb2bedba6e0
bill <bill@bill-virtual-machine.(none)>
2012-11-29 15:45:25 +0800

    dl library

---
+++ za/dl/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main()
+{
+  void *handle;
+  void (*cosine)();
+  char *error;
+  
+  handle = dlopen("/home/bill/git/billwawa/za/dl/plug.so", RTLD_LAZY);
+  if(!handle) {
+    fprintf(stderr, "%s\n", dlerror());
+    exit(1);
+  }
+  cosine = dlsym(handle, "add");
+  if((error = dlerror()) != NULL) {
+    fprintf(stderr, "%s\n", error);
+    exit(1);
+  }
+  (*cosine)();
+  dlclose(handle);
+  return 0;
+}
+
+
+
+
---
+++ za/dl/plug.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void add()
+{
+  char *argv[] = {"ls", "-al", NULL};
+  if(fork() > 0) {
+    printf("parent..\n");
+    sleep(10);
+    printf("parent..end...\n");
+    
+  } else {
+    printf("child..\n");
+    execvp("ls", argv);  
+    sleep(7);
+    printf("child..end...\n");
+//    exit(1);
+  }
+  printf("add ok!\n");
+  printf("2.add ok!\n");
+}
+
+int main()
+{
+  add();
+  return 0;
+}
+
+//execvp();
+
+
