003652d931c3d62dc37dd5e1ded2ed2f9cb1a1a0
bill <bill@ubuntu.(none)>
2013-01-08 15:41:42 +0800

    shit

--- ipc/tcp/simpletcp/client.c
+++ ipc/tcp/simpletcp/client.c
@@ -7,25 +7,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define PORT 9734
+#define PORT 7890 
 
-int main()
+int main(int argc, char **argv)
 {
-  /* create a socket */
+  // create a socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
   struct sockaddr_in address;
   address.sin_family = AF_INET;
-  address.sin_addr.s_addr = inet_addr("127.0.0.1");
-  address.sin_port = htons(PORT);
-  /* connect to the server */
+  address.sin_addr.s_addr = inet_addr((argv[1])?(argv[1]):("127.0.0.1"));
+  address.sin_port = htons((atoi(argv[2]))?atoi(argv[2]):PORT);
+  // connect to the server 
   int result = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
-  if(result == -1)
-  {
+  if (result == -1) {
     perror("connect failed: ");
     exit(1);
   }
-  /* exchange data */
+  // exchange data 
   char ch[200] = "helloworldhelloworldhelloworldhelloworldhelloworldhelloworld";
+  send(sockfd, ch, strlen(ch)+1, 0);
+  send(sockfd, ch, strlen(ch)+1, 0);
   send(sockfd, ch, strlen(ch)+1, 0);
  
     
--- ipc/tcp/simpletcp/server.c
+++ ipc/tcp/simpletcp/server.c
@@ -6,39 +6,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
-#define PORT 9734
+#define PORT 7890 
 
 int main()
 {
+  // socket 
+  int reuseflag = 1;
   int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuseflag, sizeof(int)) == -1) {
+    perror("socket reuse addr error:");
+    close(server_sockfd);
+  }
+  // local address
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   server_addr.sin_port = htons(PORT);
-  
+
+  // without error control 
   bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  listen(server_sockfd, 2);
-  char ch[19];
+  listen(server_sockfd, 5);
+
+  char ch[20];
   int client_sockfd;
   struct sockaddr_in client_addr;
   socklen_t len = sizeof(client_addr);
+
   while(1)
   {
     printf("server waiting:\n");
-    /* accept a connection */
+    // accept a connection
     client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &len);
-    /* exchange data */
-    int j;
-    for(j = 0; j < 2; j++) { 
-      recv(client_sockfd, ch, 19, 0); 
+    // receive data
+    int n = 0;
+    int nread; 
+    while((nread = recv(client_sockfd, ch, 19, 0)) > 0) {
+      ch[nread + 1] = '\0';
       printf("get char from client: %s\n", ch);
-      printf(":%d\n", j);
-      sleep(1);
-    } 
-    recv(client_sockfd, ch, 19, 0); 
-    printf("get char from client: %s\n", ch);
- //   send(client_sockfd, ch, sizeof(ch), 0);
+      n += nread; 
+      sleep(2);
+    }
+    if (nread == -1 && errno != EAGAIN) {
+       perror("read error:");
+    }
     close(client_sockfd);  
   }
   close(server_sockfd);
