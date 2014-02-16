a9dc2332308092f979618768912bdc5674d03c44
bill <wiltchamberlain@126.com>
2014-02-16 19:27:28 +0800

    something new

---
+++ linklist.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} *linklist, Node;
+
+int init()
+{
+    linklist l = (Node*)malloc(sizeof(Node));
+
+}
+
+int main()
+{
+
+    return 0;
+}
--- python/server.py
+++ python/server.py
@@ -4,4 +4,4 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", 8081))
 while True:
     data, addr = s.recvfrom(1024)
-    print "received", data, "from", addr
+    print "received\"", data, "\"from", addr
